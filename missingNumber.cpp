f010b2bdca66ca9c457ab38da02dabbce099db8e
Lambert <2359133163@qq.com>
2019-06-30 19:48:31 +0800

    include another method

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -5,36 +5,55 @@ using namespace std;
 
 class Solution {
 public:
+	//【法1】(700ms)
+	//int missingNumber(vector<int>& nums) {
+	//	int n = nums.size(); //0...n 的序列
+	//	int exit = 0;
+	//	int res = 0;
+
+	//	//对比查找缺失的数
+	//	for (int i = 0; i <= n ; i++)
+	//	{
+	//		for (int j = 0; j < n; j++)
+	//		{
+	//			if (nums[j] == i) 
+	//			{
+	//				exit = 1;
+	//				break;
+	//			}
+	//		}
+	//		if (!exit)
+	//		{
+	//			res = i;
+	//			break;
+	//		}
+	//		exit = 0; //恢复初始状态
+	//	}
+	//	return res;
+	//}
+
+	//【法2】(24ms)
 	int missingNumber(vector<int>& nums) {
 		int n = nums.size(); //0...n 的序列
-		int exit = 0;
+		int fullSum = ((0 + n)* (n + 1)) / 2 ;  //0...n 的和
+		int missSum = 0;
 		int res = 0;
 
-		//对比查找缺失的数
-		for (int i = 0; i <= n ; i++)
+		//求和
+		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < n; j++)
-			{
-				if (nums[j] == i) 
-				{
-					exit = 1;
-					break;
-				}
-			}
-			if (!exit)
-			{
-				res = i;
-				break;
-			}
-			exit = 0; //恢复初始状态
+			missSum += nums[i];
 		}
+
+		//差项
+		res = fullSum - missSum;
 		return res;
 	}
 };
 
 int main()
 {
-	vector<int> vec = { 9,6,4,2,3,5,7,0,1 };
+	vector<int> vec = { 0 };
 	Solution s;
 	int res = s.missingNumber(vec);
 	cout << res << endl;
